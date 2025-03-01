a03e7c37df37ca9e968e692f7999acaea428ae41
Simmy <dhillonharsimran192@gmail.com>
2025-03-01 11:55:03 -0500

    Java Practice Arrays

---
+++ src/PracticeArrays.java
@@ -0,0 +1,12 @@
+public class PracticeArrays {
+    public static void main(String[] args) {
+        int[] myArray = new int[5];
+        int myArray2[] = new int[5];
+        int myArray3[] = {1,2,3,4,5};
+        myArray[1] = 4;
+        myArray2[3]= 5;
+        System.out.println(myArray[1]);
+        System.out.println(myArray2[3]);
+        System.out.println(myArray3[4]);
+    }
+}
